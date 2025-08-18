7905f452154cb6f285f6893f88bf5eca69906959
Valerio Battaglia <vabatta@gmail.com>
2025-08-18 23:00:38 +0200

    fix(zod): fix ZodModuleBuilder using NestJS internal implementation

---
+++ packages/zod/examples/module.ts
@@ -0,0 +1,67 @@
+import {
+	ConfigurableModuleBuilder,
+	Inject,
+	Injectable,
+	Logger,
+	Module,
+	type OnModuleInit,
+	Optional,
+} from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { ZodConfigurableModuleBuilder } from "@proventuslabs/nestjs-zod";
+
+import z from "zod";
+
+export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN, OPTIONS_TYPE } =
+	// new ConfigurableModuleBuilder<{ timeout: number }>()
+	new ZodConfigurableModuleBuilder(z.object({ timeout: z.number().default(100) }))
+		.setExtras(
+			{
+				isGlobal: true,
+			},
+			(definition, extras) => ({
+				...definition,
+				global: extras.isGlobal,
+			}),
+		)
+		.build();
+
+@Module({})
+export class DynModule extends ConfigurableModuleClass {}
+
+@Injectable()
+export class NestedService implements OnModuleInit {
+	readonly logger = new Logger(NestedService.name);
+
+	public constructor(
+		@Optional() @Inject(MODULE_OPTIONS_TOKEN) readonly options: typeof OPTIONS_TYPE,
+	) {}
+
+	onModuleInit() {
+		this.logger.log(this.options);
+	}
+}
+
+@Module({
+	providers: [NestedService],
+})
+export class NestedModule {}
+
+@Module({
+	// @ts-expect-error
+	imports: [DynModule.register({ timeout: 'wtf' }), NestedModule],
+})
+class AppModule {}
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule);
+
+	const logger = new Logger();
+	app.useLogger(logger);
+
+	const options = app.get<typeof OPTIONS_TYPE>(MODULE_OPTIONS_TOKEN);
+
+	logger.log(options);
+}
+
+void bootstrap();
---
+++ packages/zod/src/module/utils.ts
@@ -0,0 +1,63 @@
+// biome-ignore-all lint/suspicious/noExplicitAny: NestJS monkey types compat
+// biome-ignore-all lint/style/noNonNullAssertion: NestJS monkey types compat
+
+import type {
+	FactoryProvider,
+	InjectionToken,
+	OptionalFactoryDependency,
+	Provider,
+} from "@nestjs/common";
+
+import { uid } from "uid";
+
+export const randomStringGenerator = () => uid(21);
+
+/**
+ * @param value
+ * @returns `true` if value is `OptionalFactoryDependency`
+ */
+function isOptionalFactoryDependency(
+	value: InjectionToken | OptionalFactoryDependency,
+): value is OptionalFactoryDependency {
+	return (
+		undefined !== (value as OptionalFactoryDependency).token &&
+		undefined !== (value as OptionalFactoryDependency).optional &&
+		!(value as any).prototype
+	);
+}
+
+const mapInjectToTokens = (t: InjectionToken | OptionalFactoryDependency) =>
+	isOptionalFactoryDependency(t) ? t.token : t;
+
+/**
+ *
+ * @param providers List of a module's providers
+ * @param tokens Injection tokens needed for a useFactory function (usually the module's options' token)
+ * @returns All the providers needed for the tokens' injection (searched recursively)
+ */
+export function getInjectionProviders(
+	providers: Provider[],
+	tokens: FactoryProvider["inject"],
+): Provider[] {
+	const result: Provider[] = [];
+	let search: InjectionToken[] = tokens!.map(mapInjectToTokens);
+	while (search.length > 0) {
+		const match = (providers ?? []).filter(
+			(p) =>
+				!result.includes(p) && // this prevents circular loops and duplication
+				(search.includes(p as any) || search.includes((p as any)?.provide)),
+		);
+		result.push(...match);
+		// get injection tokens of the matched providers, if any
+		search = match
+			.filter((p) => (p as any)?.inject)
+			.flatMap((p) => (p as FactoryProvider).inject!)
+			.map(mapInjectToTokens);
+	}
+	return result;
+}
+
+export function generateOptionsInjectionToken() {
+	const hash = randomStringGenerator();
+	return `CONFIGURABLE_MODULE_OPTIONS[${hash}]`;
+}
